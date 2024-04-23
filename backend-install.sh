5e0424bfdb5d5fa9b9fae9d3c81501e76f50143b
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 16:59:24 +0530

    Expense updated Project

--- backend-install.sh
+++ backend-install.sh
@@ -2,35 +2,31 @@
 
 source ./common.sh
 
-USER_VALIDATE
+check_root
 
-echo "Please Enter Password:"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf module disable nodejs -y &>>$LOGFILE
-
 dnf module enable nodejs:20 -y &>>$LOGFILE
-
 dnf install nodejs -y &>>$LOGFILE
 
 id expense &>>$LOGFILE
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo -e "The UserID Already $G Created..$N"
-else
     useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating UserID"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 
-cd /app &>>$LOGFILE
-rm -rf /app/* &>>$LOGFILE
+cd /app
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 
-cd /app
 npm install &>>$LOGFILE
 
 #check your repo and path
@@ -47,4 +43,3 @@ dnf install mysql -y &>>$LOGFILE
 mysql -h db.somustack.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 
 systemctl restart backend &>>$LOGFILE
-echo "Restart Backend Is Done.."
