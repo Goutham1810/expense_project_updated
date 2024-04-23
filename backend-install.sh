76aadf71ef8bb8cbea2ada8900c8282520470bb9
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 16:38:03 +0530

    Expense updated Project

--- backend-install.sh
+++ backend-install.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
 
 #We have to do two validations one is executing the node with root user
+source ./common.sh
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 echo "Please Enter Password:"
 read -s mysql_root_password
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "Please Connect To $R SUDO USER $N"
-    exit 1;
-else
-    echo -e "Connected To $G SUDO USER $N"
-fi
-
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-    then
-        echo -e "The Action $2 $R Failed $N"
-        exit 1;
-    else
-        echo -e "The Action $2 $G SUCCESS $N"
-    fi 
-}
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling the NodeJs Service"
