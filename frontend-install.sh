09f5216d69ef6db5a93abe0c9415d4dd78cb88a0
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 16:48:19 +0530

    Expense updated Project

--- frontend-install.sh
+++ frontend-install.sh
@@ -1,54 +1,19 @@
 #!/bin/bash
 
-#We have to do two validations one is executing the node with root user
-
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "Please Connect To $R SUDO USER $N"
-    exit 1;
-else
-    echo -e "Connected To $G SUDO USER $N"
-fi
-
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-    then
-        echo -e "The Action $2 $R Failed $N"
-        exit 1;
-    else
-        echo -e "The Action $2 $G SUCCESS $N"
-    fi 
-}
 
 dnf install nginx -y  &>>$LOGFILE
-VALIDATE $? "Installing NGINX"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enabling NGINX"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "Starting NGINX"
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading Front-end Code"
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Extraction of Frontend"
 
 cp /home/ec2-user/expense_project_shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "Copy Configurations"
 
-systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restarting NGINX"
+systemctl restart nginxd &>>$LOGFILE
+echo "Restarting NGINX"
