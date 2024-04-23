5f4a4deed2810a8f9bde462193197622da37b8ee
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 16:43:33 +0530

    Expense updated Project

--- backend-install.sh
+++ backend-install.sh
@@ -34,7 +34,7 @@ cd /app
 npm install &>>$LOGFILE
 
 #check your repo and path
-cp /home/ec2-user/expense_project_shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense_project_updated/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
